c1927196bfbbea27d45baca8252aebc0c9aa6f1b
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2023-11-12 01:08:41 +0200

    add Pexels API base Url and key

--- lib/common/constants.dart
+++ lib/common/constants.dart
@@ -1,2 +1,8 @@
 /// Zetaton logo asset path
 const logo = 'assets/images/logo.png';
+
+/// Pexels API base url
+const baseUrl = 'https://api.pexels.com/v1';
+
+/// Pexels API auth key
+const apiKey = 'D6fxj8mABEtHi4Y6MspGhhoNUV4SfIpC8gmWxgDh12MhU6GGzvTsN71p';
