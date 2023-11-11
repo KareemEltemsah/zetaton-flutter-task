27013c5cbaeb0dcefbc50bb5cce0c461ce40c732
Kareem Eltemsah <36171346+KareemEltemsah@users.noreply.github.com>
2023-11-11 17:26:54 +0200

    create user provider

---
+++ lib/models/user/user_model.dart
@@ -0,0 +1,9 @@
+import 'package:flutter/cupertino.dart';
+import 'package:zetaton_flutter_task/models/entities/user.dart';
+
+class UserModel with ChangeNotifier {
+  User? user;
+
+  UserModel();
+
+}
